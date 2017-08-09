c5f2fc09f011d5f44725ae55a16c638ec7f151bf
neonexxa <firdaus@cyberhax.club>
2017-08-09 23:53:09 +0800

    first commit

---
+++ my.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+from firebase import firebase 
+
+lightpin   = 7
+
+firebase = firebase.FirebaseApplication('https://ttp-rpi2-python-laravel.firebaseio.com',None)
+
+def setup():
+  GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+  GPIO.setup(lightpin, GPIO.OUT)     # Set Green Led Pin mode to output
+
+def Led(x):
+  if x == 0:
+    GPIO.output(lightpin, 0)
+  if x == 1:
+    GPIO.output(lightpin, 1)
+ 
+
+def loop():
+  state = 0
+  while True:
+    result = firebase.get('/devices', None)
+    if (result['rpi']):
+      GPIO.output(lightpin, 1)
+    else :
+      GPIO.output(lightpin, 0)
+
+
+def destroy():
+  GPIO.output(lightpin, GPIO.HIGH)       # Green led off
+  GPIO.cleanup()                     # Release resource
+
+if __name__ == '__main__':     # Program start from here
+  setup()
+  try:
+    loop()
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+      destroy()
